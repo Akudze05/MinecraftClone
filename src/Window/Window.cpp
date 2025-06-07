0b1fc576eb01bab39dfc4a1e21037bdbe3b187fc
Akudze05 <akudze05@gmail.com>
2025-06-07 15:10:42 +0300

    Create texture system

--- src/Window/Window.cpp
+++ src/Window/Window.cpp
@@ -11,6 +11,9 @@
 #include "Camera.h"
 #include "Window.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #define WINDOW_WIDTH 1920
 #define WINDOW_HEIGHT 1080
 
@@ -28,38 +31,69 @@ unsigned int Window::VBO = 0;
 
 GLFWwindow* Window::window;
 
+float texCoords[] = {
+    0.0f, 0.0f, // lower-left corner
+    1.0f, 0.0f, // lower-right corner
+    0.5f, 1.0f // top-center corner
+};
+
+unsigned int Window::texture = 0;
+
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 float vertices[] = {
-    //1
-    -0.3f, -0.5f, 0.0f, 1.0f, 0.4f, 0.2f,
-    0.3f, -0.5f, 0.0f, 1.0f, 1.0f, 0.2f,
-    -0.3, 0.5f, 0.0f, 1.0f, 1.0f, 0.2f,
-    0.3f, 0.5f, 0.0f, 1.0f, 0.4f, 0.2f,
-    0.3f, -0.5f, 0.0f, 1.0f, 1.0f, 0.2f,
-    -0.3f, 0.5f, 0.0f, 1.0f, 1.0f, 0.2f,
-};
+    // positions      // colors           // texture coords
+    0.5f, 0.5f, 0.0f,   1.0f, 1.0f, 1.0f, 1.0f, 1.0f, // top right
+    0.5f, -0.5f, 0.0f,  1.0f, 1.0f, 1.0f, 1.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, // bottom left
+    -0.5f, 0.5f, 0.0f,  1.0f, 1.0f, 1.0f, 0.0f, 1.0f // top left
+    };
 
 bool firstMouse = true;
+
+int texture_width, texture_height, texture_nrChannels;
+unsigned char *data = stbi_load("C:/Users/Asg/CLionProjects/untitled1/src/assets/Sprite-0001.png", &texture_width, &texture_height,
+&texture_nrChannels, 0);
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "layout (location = 1) in vec3 aColor;\n"
-"out vec3 ourColor;\n"  // Добавьте эту строку
+"layout (location = 2) in vec2 aTexCoord;\n"
+"out vec3 ourColor;\n"
+"out vec2 TexCoord;\n"
 "uniform mat4 view;\n"
 "uniform mat4 projection;\n"
-"void main()\n"
-"{\n"
+"\n"
+"void main() {\n"
 "   gl_Position = projection * view * vec4(aPos, 1.0);\n"
-"   ourColor = aColor;\n"  // Добавьте точку с запятой
+"   ourColor = aColor;\n"
+"   TexCoord = aTexCoord;\n"
 "}\0";
 
+
+
+// #version 330 core           gl_Position
+// layout (location = 0) in vec3 aPos;
+// layout (location = 1) in vec3 aColor;
+// layout (location = 2) in vec2 aTexCoord;
+// out vec3 ourColor;
+// out vec2 TexCoord;
+// void main()
+// {
+//     gl_Position = vec4(aPos, 1.0);
+//     ourColor = aColor;
+//     TexCoord = aTexCoord;
+// }
+
 const char *fragmentShaderSource = "#version 330 core\n"
-"in vec3 ourColor;\n"  // Добавьте эту строку
 "out vec4 FragColor;\n"
-"void main() {\n"
-"   FragColor = vec4(ourColor, 1.0);\n"  // Исправьте 1f на 1.0
+"in vec3 ourColor;\n"
+"in vec2 TexCoord;\n"
+"uniform sampler2D ourTexture;\n"
+"void main()\n"
+"{\n"
+"    FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);\n"
 "}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -158,15 +192,52 @@ int Window::initialize(int width, int height, const char *title) {
 
     glUseProgram(shaderProgram);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), static_cast<void *>(nullptr));
-    glEnableVertexAttribArray(0);
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    // Исправьте:
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
 
     glUseProgram(shaderProgram);
     glBindVertexArray(Window::VAO);
 
+    // ******************************  Texture  ******************************
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    Window::texture = texture;
+
+    // set the texture wrapping/filtering options (on currently bound texture)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // load and generate the texture
+    int texture_width, texture_height, texture_nrChannels;
+    unsigned char *data = stbi_load("C:/Users/Asg/CLionProjects/untitled1/src/Window/Sprite-0001-export.png", &texture_width, &texture_height,
+    &texture_nrChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture_width, texture_height, 0,
+             GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "[:ERROR:] Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(data);
+
+    // ********************************    End    ***********************************
 
     projection = glm::perspective(
     glm::radians(45.0f),
@@ -177,6 +248,9 @@ int Window::initialize(int width, int height, const char *title) {
     // Получаем location uniform-переменных (один раз!)
     Window::viewLoc = glGetUniformLocation(Window::shaderProgram, "view");
     Window::projLoc = glGetUniformLocation(Window::shaderProgram, "projection");
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 
     // Set viewport
     glViewport(0, 0, width, height);
@@ -215,6 +289,17 @@ void Window::processInput() {
 void Window::mainloop() {
     initialize(WINDOW_WIDTH, WINDOW_HEIGHT, "MineCraft");
 
+    // Set up EBO
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // Main loop rendering
     while (!isShouldClose()) {
         // Input processing
@@ -224,34 +309,32 @@ void Window::mainloop() {
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
-        camera.Speed = 60.0f * deltaTime;
-
-        // Activate shader
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-
-        glEnable(GL_DEPTH_TEST);
-
-        // --- Добавляем тут ---
-        glm::mat4 view = camera.GetViewMatrix();
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-        // --------------------
-
-        // Clear boofer color (background color)
+        // Clear buffers
         glClearColor(0.3f, 0.6f, 0.75f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // rendering here ...
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // Activate shader
+        glUseProgram(shaderProgram);
 
+        // Set up view/projection matrices
+        glm::mat4 view = camera.GetViewMatrix();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-        // Poll events and swap boofers
+        // Bind texture
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glUniform1i(glGetUniformLocation(shaderProgram, "ourTexture"), 0);
+
+        // Draw
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // Swap buffers and poll events
         swapBuffers();
         glfwPollEvents();
     }
 
-    // Clear resources
     terminate();
 }
 
